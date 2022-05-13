10a650d0f76ef436ee6ddd969b9ca075b6da2ce4
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-13 14:37:06 -0400

    completed bonus challenge to validate code entered

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState("")
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <button> {count}</button>&nbsp;
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={()=>setCount(count + "1")}>1</button>
+          <button onClick={()=>setCount(count + "2")}>2</button>
+          <button onClick={()=>setCount(count + "3")}>3</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <button onClick={()=>setCount(count + "4")}>4</button>&nbsp;
+          <button onClick={()=>setCount(count + "5")}>5</button>&nbsp;
+          <button onClick={()=>setCount(count + "6")}>6</button>&nbsp;
+        </p>
+        <p>
+          <button onClick={()=>setCount(count + "7")}>7</button>&nbsp;
+          <button onClick={()=>setCount(count + "8")}>8</button>&nbsp;
+          <button onClick={()=>setCount(count + "9")}>9</button>&nbsp;
+        </p>
+        <p>
+          <button onClick={()=>setCount("")}>Clear</button>&nbsp;
+          <button onClick={()=>setCount(count + "0")}>0</button>&nbsp;
+          <button>Enter</button>&nbsp;
+        </p>
       </header>
     </div>
   );
